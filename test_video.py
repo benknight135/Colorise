8a156a016e8202832a262442ce65aa83dc4d79ae
Ubuntu <ubuntu@ip-172-31-44-39.us-east-2.compute.internal>
2020-03-05 21:06:09 +0000

    cleanup clode

--- test_video.py
+++ test_video.py
@@ -2,11 +2,6 @@ import ml_colorise
 import image_convert
 import os
 
-# IMAGE ML
-#image_convert.video_to_images("input/vid.mp4","input")
-#ml_colorise.test("input","output",ml_colorise.ML_TYPE_TF,ml_colorise.ML_MODE_COLORIZE)
-#image_convert.images_to_video("output")
-
 # VIDEO ML
 # create output folder if it does not exist
 if not os.path.exists("output"): 
